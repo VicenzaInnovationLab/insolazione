03c35940865a12bb5108123adc387a100132794b
Yaroslav Vasyunin <yaroslav@digitalinnovationhubvicenza.it>
2021-02-19 12:32:53 +0100

    Add confini VI

--- webmap/static/app.js
+++ webmap/static/app.js
@@ -1,36 +1,86 @@
-var geojsonFeature = "data/edifici.geojson"
-var cities = L.layerGroup();
+function onEachFeature(feature, layer) {
+    var popupContent = "<ul><li>Questo edificio può generare <b>" +
+            Math.round(feature.properties.elec_prod_mwh) + " MW·h</b> da fotovoltaici in un anno</li>" +
+            "<li>Codice edificio: <b>" + feature.properties.codice_edificio + "</b></li>" +
+            "<li>Uso: <b>" + feature.properties.uso + "</b></li></ul>";
+    layer.bindPopup(popupContent);
+}
 
-L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.').addTo(cities),
-    L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.').addTo(cities),
-    L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(cities),
-    L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(cities);
+function stileEdifici(feature) {
+        var val = feature.properties.elec_prod_mwh;
+        if (val < 50) {
+            return {color: "#000000", fillColor: "#fef0d9", opacity: 1.0, fillOpacity: 0.8, weight: 0.4};
+        } else if (50 <= val && val < 200) {
+            return {color: "#000000", fillColor: "#fdcc8a", opacity: 1.0, fillOpacity: 0.8, weight: 0.4};
+        } else if (200 <= val && val < 800) {
+            return {color: "#000000", fillColor: "#fc8d59", opacity: 1.0, fillOpacity: 0.8, weight: 0.4};
+        } else if (800 <= val && val < 3200) {
+            return {color: "#000000", fillColor: "#e34a33", opacity: 1.0, fillOpacity: 0.8, weight: 0.4};
+        } else if (3200 <= val && val < 12000) {
+            return {color: "#000000", fillColor: "#b30000", opacity: 1.0, fillOpacity: 0.8, weight: 0.4};
+        }
+}
+
+var edifici = L.geoJSON(edificiInsolazione,
+    {style: stileEdifici, onEachFeature: onEachFeature,
+    attribution: "Elaborazione Digital Surface Model MATTM - Geoportale nazionale &copy; " +
+        "<a href=\"https://digitalinnovationhubvicenza.it\">Digital Innovation Hub Vicenza</a>"});
+
+var vicenza = L.geoJSON(confiniVi,
+    {style: {color: "#ccc", opacity: 0.9, fillOpacity: 0.0, weight: 2},
+    attribution: "Confini di Vicenza &copy; " +
+        "<a href=\"http://sit.comune.vicenza.it/SitVI/vicenza/index.php\">SitVI</a>"});
 
 var mbToken = "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
-var mbAttr = "Map data &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, " +
-    "Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+var mbAttr = "Dati cartografici &copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors, " +
+    "Immagini satellitari © <a href='https://www.mapbox.com/'>Mapbox</a>",
     mbUrl = "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=" + mbToken;
 
 var dark = L.tileLayer(mbUrl, {id: 'mapbox/dark-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
     strade = L.tileLayer(mbUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mbAttr}),
     satellite = L.tileLayer(mbUrl, {id: 'mapbox/satellite-v9', tileSize: 512, zoomOffset: -1, attribution: mbAttr});
-;
 
-var map = L.map('map', {
-    center: [45.548, 11.548,], // Vicenza
+var corner1 = L.latLng(45.4335, 11.4307),
+    corner2 = L.latLng(45.6519, 11.6769),
+    bounds = L.latLngBounds(corner1, corner2);
+
+var map = L.map("map", {
+    center: [45.548, 11.548], // Vicenza
     zoom: 14,
-    layers: [dark, cities]
+    minZoom: 13,
+    maxZoom: 18,
+    maxBounds: bounds,
+    layers: [dark, vicenza, edifici]
 });
 
 var baseLayers = {
-    "Dark": dark,
+    "Basemap scuro": dark,
     "Strade": strade,
     "Satellite": satellite
 };
 
 var overlays = {
-    "Cities": cities
+    "Edifici": edifici,
+    "Limiti amministrativi": vicenza
 };
 
 L.control.layers(baseLayers, overlays).addTo(map);
-L.geoJSON(geojsonFeature).addTo(map);
+
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create("img");
+
+        img.src = "static/images/logo.png";
+        img.style.width = "300px";
+
+        return img;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+
+L.control.watermark = function(opts) {return new L.Control.Watermark(opts);}
+L.control.watermark({ position: "bottomright" }).addTo(map);
+L.control.scale().addTo(map);
